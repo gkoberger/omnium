cf37e938e2bc095ae60da81326be3b185b281eda
Gregory Koberger <gkoberger@gmail.com>
2011-07-04 11:45:57 -0700

    Lots of changes; not sure what

---
+++ .builder/jetpack/wrapper.js
@@ -0,0 +1,59 @@
+var omniumWrapper = function(){
+    function randomNum() {
+        return Math.floor(Math.random() * 10000);
+    }
+
+    var callbacks = {};
+
+    var Storage = function(){
+        this.save = function(arg1, arg2, arg3) {
+            var cid = randomNum(),
+                message = {'action': 'storage_set', 'cid': cid},
+                callback = function(){};
+
+            if(typeof arg1 == 'object') {
+                message['values'] = arg1;
+                if(typeof arg2 == 'function') {
+                    callback = arg2;
+                }
+            } else {
+                message['values'] = {};
+                message['values'][arg1] = arg2;
+                if(typeof arg3 == 'function') {
+                    callback = arg3;
+                }
+            }
+
+            callbacks[cid] = callback;
+            postMessage(message);
+        }
+
+        this.request = function(keys, callback) {
+            callback = callback || function(){};
+
+            var cid = randomNum();
+
+            callbacks[cid] = callback;
+            postMessage({'cid': cid, 'action': 'storage_get', 'keys': keys});
+        }
+
+        this.remove = function(name) {
+            ss.storage[name] = null;
+        }
+    }
+    this.storage = new Storage();
+    on('message', function(r) {
+        var cid = r.cid,
+            message = undefined;
+
+        if(typeof r.message != "undefined") {
+            message = r.message;
+        }
+
+        callbacks[cid](message);
+    });
+
+};
+
+_ = new omniumWrapper();
+
